58755d2056089a2f9f63d31a0e7a7c0416f7c7ec
Copy Liu <copyliu@gmail.com>
2021-04-17 22:04:27 +0800

    添加项目文件。

---
+++ IPTVWebViewer/Controller/PlaylistController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using PlaylistsNET.Content;
+using PlaylistsNET.Models;
+
+namespace IPTVWebViewer.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlaylistController : ControllerBase
+    {
+        public class PlayListEntry
+        {
+            public string name { get; set; }
+            public string url { get; set; }
+        }
+
+        private readonly IWebHostEnvironment _appEnvironment;
+
+        public PlaylistController(IWebHostEnvironment appEnvironment)
+        {
+            _appEnvironment = appEnvironment;
+        }
+        [HttpGet]
+        [Route("all")]
+        public List<PlayListEntry> GetPlayList()
+        {
+            var playlist = Path.Combine(_appEnvironment.WebRootPath, "playlist.m3u");
+            var parser=PlaylistsNET.Content.PlaylistParserFactory.GetPlaylistParser(PlaylistType.M3U8);
+            using var file = new FileStream(playlist, FileMode.Open);
+            var playlists=(M3uPlaylist)parser.GetFromStream(file);
+            return playlists.PlaylistEntries.Select((p,i) => new PlayListEntry()
+            {
+                name = p.Title + playlists.Comments.Skip(i).FirstOrDefault(),
+                url = p.Path
+            }).Where(p => p.url.StartsWith("rtp://")).ToList();
+
+        }
+    }
+}
---
+++ IPTVWebViewer/Controller/StreamController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using FFMpegCore.Enums;
+using FFMpegCore.Pipes;
+using Microsoft.Net.Http.Headers;
+
+namespace IPTVWebViewer.Controller
+{
+    [Route("api/stream")]
+    [ApiController]
+    public class StreamController : ControllerBase
+    {
+        [Route("{rtpurl}")]
+        [HttpGet]
+        public async Task RtpStream(string rtpurl, CancellationToken cancellationToken)
+        {
+            this.Response.StatusCode = 200;
+            this.Response.Headers.Add(HeaderNames.ContentDisposition, $"attachment; filename=\"stream\"");
+            this.Response.Headers.Add(HeaderNames.ContentType, "application/octet-stream");
+            var pipe = new StreamPipeSink(this.Response.Body);
+            var arg=FFMpegCore.FFMpegArguments.FromFileInput($"rtp://{rtpurl}", false).OutputToPipe(pipe, options =>
+
+            {
+                options.ForceFormat("mpegts");
+                options.WithAudioCodec("aac");
+                options.WithVideoCodec("copy");
+                options.WithFastStart();
+                options.UsingMultithreading(true);
+                options.WithCustomArgument("-analyzeduration 500000");
+                options.WithCustomArgument("-fflags nobuffer");
+                options.WithCustomArgument("-flags low_delay");
+                options.WithCustomArgument("-max_delay 250000");
+                options.WithCustomArgument("-max_interleave_delta 1"); // see https://github.com/l3tnun/EPGStation/issues/433
+
+            });
+            arg.CancellableThrough(out var cancel);
+            cancellationToken.Register(cancel);
+            await arg.ProcessAsynchronously();
+
+        }
+    }
+}
